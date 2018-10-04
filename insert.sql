4c4801d25189ff51c0fb8126ac4fa651fa7c392f
Mr Booi <ayabongabooi2@gmail.com>
2018-10-04 15:22:36 +0200

    database schema design for leave_days

---
+++ insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO leave_type (leave_type) VALUES ('Sick leave');
+INSERT INTO leave_type (leave_type) VALUES ('unpaid leave');
+INSERT INTO leave_type (leave_type) VALUES ('paid leave');
--- leave-days.sql
+++ leave-days.sql
@@ -1,4 +1,4 @@
-drop table if exists request_leave, users, leave_type;
+drop table if exists user_leave_allowed,leave_status,leave_request, users, leave_type;
 
 CREATE TABLE users
 (
@@ -7,12 +7,9 @@ CREATE TABLE users
     last_name VARCHAR(100) NOT NULL,
     position text not NULL,
     email text UNIQUE NOT NULL,
-    PASS_WORD VARCHAR(100) Not NULL
-    -- joined TIMESTAMP NOT NULL DEFAULT TIMESTAMP
+    user_password text Not NULL,
+    joined_date TIMESTAMP NOT NULL 
 );
-
-INSERT INTO users (first_name,last_name,position,email,PASS_WORD) VALUES ('Ayabonga','Booi','fullstack dev','aya@gmail','123');
-INSERT INTO users (first_name,last_name,position,email,PASS_WORD) VALUES ('Luvuyo','Sono','fullstack dev','sono@gmail','1234');
 
 SELECT *
 FROM users;
@@ -20,34 +17,45 @@ FROM users;
 CREATE TABLE leave_type
 (
     id serial PRIMARY KEY,
-     leave_type  VARCHAR(100) NOT NULL,
-     amount int DEFAULT 5
+     leave_type  text NOT NULL
+     
     -- Study_leave int DEFAULT 5,
     -- unpaid_leave int DEFAULT 10,
     -- family_responsibility int DEFAULT 3,
     -- paid_leave int DEFAULT 17
 );
 
-INSERT INTO leave_type (leave_type) VALUES ('Sick leave');
-INSERT INTO leave_type (leave_type) VALUES ('unpaid leave');
-INSERT INTO leave_type (leave_type) VALUES ('paid leave');
-
 SELECT *
 FROM leave_type;
 
-CREATE TABLE request_leave
+CREATE TABLE leave_request
 (
     id serial PRIMARY KEY,
-    leave_id int not null,
-    users_id int not null,
-    Description VARCHAR(250) not null,
-    start_date text not null,
-    end_date text not null,
-    status VARCHAR DEFAULT 'Pending',
-    foreign key (leave_id) references request_leave(id),
-    foreign key (users_id) references users(id)
+    leave_type_id int not null,
+    user_id int not null,
+    leave_description text not null,
+    start_date date not null,
+    end_date date not null,
+    status_id int not NULL,
+    foreign key (leave_type_id ) references request_leave(id),
+    foreign key (user_id) references users(id),
+    foreign key (status_id) references leave_status(id)
+)
+
+CREATE TABLE leave_status(
+  id serial PRIMARY KEY,
+  status text DEFAULT 'Pending'
+)
+
+CREATE TABLE user_leave_allowed(
+    id serial PRIMARY KEY,
+    user_id int not null,
+    leave_type_id int not null,
+    leave_amount int not null,
+    foreign key (user_id) references users(id),
+    foreign key (leave_type_id) references leave_type(id)
 )
 
 
--- SELECT * FROM request_leave;
+
 
